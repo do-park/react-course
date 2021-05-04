c3ae0fe572c39818e4a7ab271c1df9d61bb99200
박도희 <dohee.pa@gmail.com>
2021-05-04 21:36:13 +0900

    31. Adding Links & Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import FavoritesPage from './pages/Favorites';
+import NewMeetupPage from './pages/NewMeetup';
+
 function App() {
   return (
     <div>
+      <Switch>
+        <Route path="/">
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
