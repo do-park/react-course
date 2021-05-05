da6e81abddac62fefae529df34dec406e99440e3
박도희 <dohee.pa@gmail.com>
2021-05-06 08:19:55 +0900

    Fix missing code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Layout>
       <Switch>
-        <Route path="/">
+        <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
         <Route path="/new-meetup">
